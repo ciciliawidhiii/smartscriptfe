52cc1ade353188076abd02dec6ff6a2b7e36e4fa
ciciliawidhiii <ciciliawidhi1@gmail.com>
2023-01-16 16:08:10 +0700

    add styling

--- smartscripts/src/pages/Login.jsx
+++ smartscripts/src/pages/Login.jsx
@@ -13,11 +13,11 @@ export default function Login() {
       <div className="bg-yellow-800 flex flex-col justify-center">
         <form className="max-w[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounde-lg"> 
           <h2 className="text-4xl dark:text-white font-bold text-center">Selamat Datang di Pusat Bantuan LPDP!</h2>
-          <div>
+          <div className="flex flex-col text-gray-400 py-2">
             <label>Username</label>
-            <input type="text" placeholder='Masukkan username anda'/>
+            <input className= 'rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" placeholder='Masukkan username anda'/>
           </div>
-          <div>
+          <div className="flex flex-col text-gray-400 py-2">
             <label>Password</label>
             <input type="password" placeholder='Masukkan password Anda'/>
           </div>
